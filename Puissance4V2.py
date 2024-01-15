9fce2be6cc87726fd6f1c7c27b86a60c8dd7034f
n-sld-44 <148266212+n-sld-44@users.noreply.github.com>
2024-01-15 14:47:51 +0100

    Update Puissance4V2.py

--- Puissance4V2.py
+++ Puissance4V2.py
@@ -6,8 +6,8 @@ def pause():
     programPause = input("Press the <ENTER> key to continue...")
 
 
-largeur = 4
-hauteur = 4
+largeur = 7
+hauteur = 6
 
 
 def newGame():
@@ -49,18 +49,39 @@ def terminalTest(grille, pions ):
             if grille[j][i] == grille[j+1][i] == grille[j+2][i] == grille[j+3][i] != '.':
                 return True 
     #Diagonales
-    for i in range(hauteur-3):
-        for j in range(largeur -3):
+    for i in range(largeur-3):
+        for j in range(hauteur -3):
             if grille[i][j] == grille[i+1][j+1] == grille[i+3][j+3] == grille[i+2][j+2] != '.':
                 return True 
-    for i in range(3,hauteur):
-        for j in range(largeur-3):
+    for i in range(3,largeur):
+        for j in range(hauteur-3):
             if grille[i][j] == grille[i-1][j+1] == grille[i-3][j+3] == grille[i-2][j+2] != '.' :
                 return True
     return False
 
 
-
+def scoreWindow(window,joueur):
+    
+    score = 0
+    if joueur == 'X':
+        adv = 'O'
+    else:
+        adv = 'X'
+        
+    if window.count(joueur) == 4:
+        score += 100
+    elif window.count(joueur) == 3 and window.count('.')==1:
+        score += 5
+    elif window.count(joueur) == 2 and window.count('.')==2:
+        
+        score+=2
+    if window.count(adv) == 3  and window.count('.') == 1:
+        score -= 4
+    return score 
+        
+        
+    
+    
             
 def actions(grille):
     listeaction = []
@@ -80,23 +101,42 @@ def resultReset(grille,colonne):
             break
     return grille 
     
-def utility(jeu,pions,tt):
-    if tt:
-        if pions[0] == 'X':
-            return -22+pions[1]//2
-        if pions[1]== 'O':
-            return 21 - pions[1]//2
-        else:
-            return 0 
+def utility(jeu,joueur):
+    score = 0
+    #Horizontal
+    for li in range(hauteur):
+        lignes = [i for i in list(jeu[:][li])]
+        for col in range(largeur-3):
+            window = lignes[col:col+4]
+            score += scoreWindow(window,joueur)
+    #Vertical
+    for col in range(largeur):
+        colonne = [i for i in list(jeu[col][:])]
+        for li in range(hauteur-4):
+            window = colonne[li:li+4]
+            score += scoreWindow(window,joueur)
+    # Diago montante
+    for li in range(hauteur-3):
+        for col in range(largeur-3):
+            window =[jeu[col+i][li+i] for i in range(4)]
+            score += scoreWindow(window,joueur)       
+    #Diago Descendante
+    for li in range(3,hauteur):
+        for col in range(largeur-3):
+            window = [jeu[col+i][li-i] for i in range(4)]
+            score += scoreWindow(window, joueur)
+    
+    return score
+    
+
+
         
     
 
 #%% 
 
 def minimax(jeu,profondeur,maximise):
-    '''affichage(jeu)
-    print(jeu)
-    pause()'''
+    
     pions = joueur(jeu)
     '''if profondeur ==0:
             return utility(jeu,pions)'''
@@ -124,15 +164,61 @@ def minimax(jeu,profondeur,maximise):
             jeu = resultReset(jeu,a)
         return best
 
-def meilleurcoup(jeu,profondeur = 10):
-    player = joueur(jeu)[0]
-    if player == 'X':
-        best_val = -math.inf
-        best_move = None 
+
+
+
+def elagage(jeu,profondeur,maximise,alpha,beta):
+    
+    pions = joueur(jeu)
+   
+    if pions[1] == 42 or pions[1] == largeur* hauteur:
+        return 0
+    tt = terminalTest(jeu,pions[0])
+    if tt:
+        return -100 if pions[0]=='X' else 100
+    if profondeur ==0:
+        affichage(jeu)
+        score =utility(jeu,pions[0])
+        print(score)
+        return score
+     
+    
+    if maximise:
+        best = -math.inf
         act = actions(jeu)
         for a in act:
             jeu = result(jeu,a)
-            move_val = minimax(jeu, profondeur-1,False)
+            best = max(best,elagage(jeu,profondeur-1,False,alpha,beta))
+            jeu = resultReset(jeu,a)
+            alpha = max(alpha,best)
+            if alpha>= beta:
+                break
+        return best
+    
+    
+    else:
+        best = math.inf
+        act = actions(jeu)
+        for a in act:
+            jeu = result(jeu, a)
+            best = min(best, elagage(jeu,profondeur -1,True,alpha,beta))
+            jeu = resultReset(jeu,a)
+            beta = min(beta,best)
+            if alpha>= beta:
+                break
+        return best
+
+def meilleurcoup(jeu,profondeur = 8):
+    alpha = -math.inf
+    beta = math.inf
+    player = joueur(jeu)[0]
+    if player == 'X':
+        best_val = -math.inf
+        
+        act = actions(jeu)
+        for a in act:
+            jeu = result(jeu,a)
+            move_val = elagage(jeu, profondeur-1,False,alpha,beta)
             jeu = resultReset(jeu, a)
             if move_val>best_val:
                 best_val = move_val
@@ -142,11 +228,11 @@ def meilleurcoup(jeu,profondeur = 10):
     
     if player == 'O':
         best_val = math.inf
-        best_move = None 
+        
         act = actions(jeu)
         for a in act:
             jeu = result(jeu,a)
-            move_val = minimax(jeu, profondeur-1,True)
+            move_val = elagage(jeu, profondeur-1,True,alpha,beta)
             jeu = resultReset(jeu, a)
             if move_val<best_val:
                 best_val = move_val
@@ -154,12 +240,17 @@ def meilleurcoup(jeu,profondeur = 10):
             print(best_move,best_val)
         return best_move 
 
+
+
+
+
+
 def jeuIA(jeu = None):
     if jeu == None:
         jeu = newGame()
     affichage(jeu)
     while True:
-        coup = meilleurcoup(jeu,100)
+        coup = meilleurcoup(jeu,7)
         jeu = result(jeu,coup)
         affichage(jeu)
         j = joueur(jeu)
@@ -179,5 +270,5 @@ def jeuIA(jeu = None):
         if terminalTest(jeu,j[1]):
             print(j[0] + ' a perdu')
             break
-jeuIA([['X', 'O', '.', '.'], ['X', 'O', '.', '.'], ['O', 'X', '.', '.'], ['X', 'O', '.', '.']])
+jeuIA()
     
